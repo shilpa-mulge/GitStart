ecfec421ec24ec0390f3e4f56426074af9c336f3
shilpa-mulge <mulgeshilpa@gmail.com>
2022-11-16 13:51:09 +0530

    Dom with TagName commit

--- index1.js
+++ index1.js
@@ -1,4 +1,4 @@
-const a=document.getElementsByClassName('list-group-item')
+const a=document.getElementsByTagName('li')
 a[2].style.backgroundColor="green"
 for(var i=0;i<a.length;i++){
     a[i].style.color="red"
