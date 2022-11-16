093630f69dcc431f6428b96aad520e895c6b05db
shilpa-mulge <mulgeshilpa@gmail.com>
2022-11-16 13:44:32 +0530

    Dom 2 commit

---
+++ index1.js
@@ -0,0 +1,6 @@
+const a=document.getElementsByClassName('list-group-item')
+a[2].style.backgroundColor="green"
+for(var i=0;i<a.length;i++){
+    a[i].style.color="red"
+    a[i].style.fontWeight="bold"
+}
